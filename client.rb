a1b16438bec33d41a590245366a81479886629bb
Mateus Cavedini <mateuscvdn@gmail.com>
2025-04-30 22:04:52 -0300

    implement CLI chat client

---
+++ client.rb
@@ -0,0 +1,32 @@
+require 'socket'
+
+module Chat
+  class Client
+    def initialize(settings)
+      @host = settings[:host]
+      @port = settings[:port]
+      @client = Socket.new :INET, :STREAM
+    end
+
+    def start
+      @client.connect Socket.pack_sockaddr_in @port, @host
+
+      print 'Insert your username: '
+      username = gets.chomp
+      @client.puts username
+
+      Thread.new do
+        loop { puts @client.gets }
+      end
+
+      loop do
+        command_str = gets.chomp
+        print "\e[A\e[K"
+        @client.puts command_str
+      end
+    end
+  end
+end
+
+Chat::Client.new(host: '127.0.0.1', port: 3000).start
+
